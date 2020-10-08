24339f9a9a71300e9afa23baad734c26acbceec2
Sm Al-amin <67338203+alaminstore@users.noreply.github.com>
2020-10-09 03:35:33 +0600

    Update MyCard.js

--- src/components/MyCard.js
+++ src/components/MyCard.js
@@ -1,11 +1,47 @@
 import React from "react";
-
-function MyCard() {
-  return (
-    <div>
-      <h2>Mycard</h2>
-    </div>
-  );
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Grid,
+  Typography,
+} from "@material-ui/core";
+function MyCard({ match }) {
+  const getMatchCard = () => {
+    return (
+      <Card style={{ marginTop: 20 }}>
+        <CardContent>
+          <Grid container justify="center" spacing={5} alignItems="center">
+            <Grid item>
+              <Typography variant="h6">{match}</Typography>
+            </Grid>
+            <Grid item>
+              <img
+                style={{ width: 85 }}
+                src={require("../img/vs.png")}
+                alt="vs"
+              />
+            </Grid>
+            <Grid item>
+              <Typography variant="h6">Second Team</Typography>
+            </Grid>
+          </Grid>
+        </CardContent>
+        <CardActions>
+          <Grid container justify="center">
+            <Button variant="contained" color="primary">
+              See Details
+            </Button>
+            <Button variant="contained" color="primary">
+              {new Date().toString()}
+            </Button>
+          </Grid>
+        </CardActions>
+      </Card>
+    );
+  };
+  return getMatchCard();
 }
 
 export default MyCard;
