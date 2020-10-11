7abe0a1f0491b0df6c81c814cc052b980464489a
Sm Al-amin <67338203+alaminstore@users.noreply.github.com>
2020-10-11 18:46:46 +0600

    Update Api.js

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,10 +1,18 @@
-import React from "react";
 const Api_Key = "8p0e9XqJ1oQMiwfb24sLOdoNSVh1";
 function getMatches() {
   const url = `https://cricapi.com/api/matches?apikey=${Api_Key}`;
   return fetch(url)
     .then((response) => response.json())
     .catch((error) => console.log("Error: ", error));
+}
+
+
+
+export const getMatchesDetails= (id) => {
+  const url = `https://cricapi.com/api/cricketScore?apikey=${Api_Key}&unique_id=${id}`
+  return fetch(url)
+    .then(response=>response.json())
+    .catch(error=>console.log('Error: ',error)) 
 }
 
 export default getMatches;
